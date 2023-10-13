dedd3cddbf705a47fe19bc97f927152fb28ff2fd
malachi2237 <35237626+malachi2237@users.noreply.github.com>
2023-10-13 12:07:33 -0700

    Made Enemy class to be parent to enemies

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -1,16 +1,25 @@
 extends Area2D
 
+class_name Enemy
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+onready var hitbox = $EnemyBox
 
+export(float) var health = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	monitoring = true
 
+func _on_Enemy_area_entered(area):
+	if ("damage" in area):
+		_take_damage(area.damage)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _take_damage(var damage: int) -> void:
+	health -= damage
+	if (health <= 0):
+		_die()
+	else:
+		print("Hit!")
+
+func _die() -> void:
+	queue_free()
