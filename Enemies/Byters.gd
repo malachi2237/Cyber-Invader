ed477ce0d185f0358de2c80bf05cc8ec2df73a8c
Agreafel <25335635+Agreafel@users.noreply.github.com>
2023-10-13 14:09:57 -0700

    fixed type warnings

--- Enemies/Byters.gd
+++ Enemies/Byters.gd
@@ -1,15 +1,15 @@
 extends Enemy
 
-export(float) var movement_angle = 20
-export(float) var movement_speed = 50
+export(float) var movement_angle = 20.0
+export(float) var movement_speed = 50.0
 export(bool) var facing_right = true
 
 func _process(delta) -> void:
 	var direction = Vector2.RIGHT.rotated(deg2rad(movement_angle))
 	direction.normalized()
-	
+
 	if (!facing_right):
 		direction.x = -direction.x
-		
+
 	translate(direction * movement_speed * delta)
-	
+
