185e152b39c7c44b5e679cd62f85e55a5ddd3e93
Agreafel <25335635+Agreafel@users.noreply.github.com>
2023-10-13 14:13:06 -0700

    cleanup before afk

--- Bullets/BulletSpawner.gd
+++ Bullets/BulletSpawner.gd
@@ -9,25 +9,25 @@ onready var bullet_prefab = preload("res://Bullets/Bullet.tscn")
 var shoot_timer
 var rotator
 
-export(float) var initial_rotation = 0
-export(float) var rotation_speed = 100
+export(float) var initial_rotation = 0.0
+export(float) var rotation_speed = 100.0
 export(float) var shot_timer = .2
-export(float) var radius = 100
+export(float) var radius = 100.0
 
 export(int) var spawn_point_count = 1
 
 func _ready():
 	var step = 2 * PI / spawn_point_count
-	
+
 	if (bullet_override):
 		bullet_prefab = load(bullet_override)
-		
+
 	shoot_timer = Timer.new()
 	rotator = Node2D.new()
-	
+
 	add_child(shoot_timer)
 	add_child(rotator)
-	
+
 
 	for i in range(spawn_point_count):
 		var spawn_point = Node2D.new()
@@ -35,9 +35,9 @@ func _ready():
 		spawn_point.position = pos
 		spawn_point.rotation = pos.angle()
 		rotator.add_child(spawn_point)
-	
+
 	rotator.rotate(deg2rad(initial_rotation))
-	
+
 	shoot_timer.wait_time = shot_timer
 	shoot_timer.connect("timeout", self, "_volley")
 	shoot_timer.start()
@@ -45,14 +45,14 @@ func _ready():
 func _process(delta):
 	var new_rotation = rotator.rotation_degrees + rotation_speed * delta
 	rotator.rotation_degrees = fmod(new_rotation, 360)
-	
+
 func _volley() -> void:
 	for s in rotator.get_children():
 		var bullet = bullet_prefab.instance()
 		var root = get_tree().get_root()
 		var scene = root.get_child(root.get_child_count()-1)
-		
+
 		scene.add_child(bullet)
-		
+
 		bullet.position = s.global_position
 		bullet.rotation = s.global_rotation + deg2rad(90)
