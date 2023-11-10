90ffaccd8ee32056e3665455a289e09ff516af1b
Agreafel <25335635+Agreafel@users.noreply.github.com>
2023-11-10 11:33:15 -0800

    more clean on gamemanager

--- Level/Scripts/GameManager.gd
+++ Level/Scripts/GameManager.gd
@@ -16,14 +16,14 @@ func _switch_phase():
 	if !stage_queue.empty():
 		var next_scene: PackedScene = stage_queue.pop_front()
 		var phase_instance: LevelPhase = next_scene.instance()
-
 		var root = get_tree().get_root()
 		var current_scene = root.get_child(root.get_child_count()-1)
 
 		current_scene.add_child(phase_instance)
+		connectPhase(phase_instance)
 
-		var _connectPhase = phase_instance.connect(
-			"phase_ended", self, "_switch_phase")
+func connectPhase(phase_instance: LevelPhase) -> void:
+	var _toss = phase_instance.connect("phase_ended", self, "_switch_phase")
 
 func _switchScene() -> void:
 	pass
