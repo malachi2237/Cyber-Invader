809c91a4fd7a72ad56abd5fb1b497687653be213
agreafel <25335635+Agreafel@users.noreply.github.com>
2023-11-13 13:55:37 -0800

    ModesForBoss

--- Enemies/Boss.gd
+++ Enemies/Boss.gd
@@ -1,16 +1,47 @@
 extends Enemy
 
+#Has the player as a target
+#Has three lazer guns
+var target = null
+onready var Guns = [$Bosscannon1, $Bosscannon2, $Bosscannon3]
+onready var modeTimer: Timer = $ModeTimer
+onready var maxHealth = self.health
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var modeCount:int = 3
+export var modeLength = [30, 30]
+export var healthMilestones = [0.6, 0.3]
+export var modes = ["CirclePulse", "Sweeping", "HyperSnow"]
+var currentMode = 0
 
+#Has three modes
+#Mode 1: Pulses of circuling bullet
+#Mode 2: Shifting Blocks and Sweeping Lazers
+#Mode 3: Crazy Bullet snow, attop combinations of the previous patterns
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	startModeTimer()
 
+func nextModeValid() -> bool:
+	return currentMode < modeCount -1
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func healthMilestoneReached() -> bool:
+	return (self.health/maxHealth) <= healthMilestones[currentMode] 
+
+#Can move between modes via Health milestones
+func healthModeSwitchTest() -> void:
+	if(nextModeValid() and healthMilestoneReached()): startNextMode()
+
+func startNextMode() -> void:
+	if(nextModeValid()): 
+		currentMode += 1
+		startModeTimer()
+
+func startModeTimer() -> void:
+	modeTimer.one_shot = true
+	modeTimer.wait_time = modeLength[currentMode]
+	modeTimer.start()
+
+#Has a timer that moves between Modes
+func _on_ModeTimer_timeout():
+	startNextMode()
