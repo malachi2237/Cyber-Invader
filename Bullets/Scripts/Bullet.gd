51db8da1b046fd66bd85da65b0b4632969886d63
Agreafel <25335635+Agreafel@users.noreply.github.com>
2023-11-15 21:38:20 -0800

    renamedWavePhaseVariables

--- Bullets/Scripts/Bullet.gd
+++ Bullets/Scripts/Bullet.gd
@@ -7,10 +7,7 @@ export var is_from_player = false
 
 export var shot_speed = 600
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+#---------------------------------------------
 func _physics_process(delta: float) -> void:
 	_apply_movement(delta)
 
@@ -19,3 +16,4 @@ func _on_EndBullet_timeout() -> void:
 
 func _apply_movement(var delta: float) -> void:
 	position.y -= shot_speed*delta
+#---------------------------------------------
--- Level/Scripts/WavePhase.gd
+++ Level/Scripts/WavePhase.gd
@@ -3,56 +3,50 @@ extends LevelPhase
 class_name WavePhase
 
 export(bool) var auto_start = false
-export(Array, PackedScene) var spawn_group_stack: Array
-export(Array, NodePath) var spawn_point_stack: Array
-export(Array, float) var spawn_delay_stack: Array
+export(Array, PackedScene) var spawn_groups:Array
+export(Array, NodePath) var spawn_points: Array
+export(Array, float) var spawn_delays: Array
 
-var spawn_timer: Timer
+onready var spawn_timer: Timer = $SpawnTimer
 
 func nonZeroCondition() -> bool:
-	return !spawn_point_stack.empty()
+	return !spawn_points.empty()
 
 func equalSizeCondition() -> bool:
-	return (spawn_point_stack.size() == spawn_group_stack.size() and
-		 spawn_point_stack.size() == spawn_delay_stack.size())
+	return (spawn_points.size() == spawn_groups.size() and
+		 spawn_points.size() == spawn_delays.size())
 
 func phaseError() -> void:
 	printerr("Spawn stacks do not contain the same amount of elements")
 	queue_free()
 
-func makePhase() -> void:
-	spawn_timer = Timer.new()
-	add_child(spawn_timer)
-	spawn_timer.one_shot = true
-	connectSpawnTimer()
+func autoStartPhase() -> void:
 	if auto_start: _start_phase()
 
 func _ready():
-	if nonZeroCondition() and equalSizeCondition():
-		makePhase()
-	else:
-		phaseError()
-
-func connectSpawnTimer() -> void:
-	var _toss = spawn_timer.connect("timeout", self, "_spawn_next_group")
+	if (nonZeroCondition() and equalSizeCondition()): autoStartPhase()
+	else: phaseError()
 
 func _start_phase():
-	spawn_timer.start(spawn_delay_stack.pop_front())
+	spawn_timer.start(spawn_delays.pop_front())
 
 func _spawn_next_group():
-	var spawn_node: Node2D = get_node(spawn_point_stack.pop_front())
+	var spawn_node: Node2D = get_node(spawn_points.pop_front())
 	var pos = spawn_node.global_position
 
-	var group_instance = spawn_group_stack.pop_front().instance()
+	var group_instance = spawn_groups.pop_front().instance()
 
-	var root = get_tree().get_root()
-	var current_scene = root.get_child(root.get_child_count()-1)
+	var current_scene = Utility.getScene(self)
 
 	current_scene.add_child(group_instance)
 	group_instance.global_position = pos
 
-	if spawn_delay_stack.empty():
+	if spawn_delays.empty():
 		_end_phase()
 		queue_free()
 	else:
-		spawn_timer.start(spawn_delay_stack.pop_front())
+		_start_phase()
+
+
+func _on_SpawnTimer_timeout() -> void:
+	_spawn_next_group()
