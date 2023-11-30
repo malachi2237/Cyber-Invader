bfc7220ad4caf410510e66441ef23c9349857f13
Agreafel <25335635+Agreafel@users.noreply.github.com>
2023-11-30 15:32:51 -0800

    ClearerVision

--- Bullets/Scripts/SpawnPointsContainer.gd
+++ Bullets/Scripts/SpawnPointsContainer.gd
@@ -2,34 +2,26 @@ extends Node2D
 
 export(int) var spawn_point_count = 1
 
-#TODO: (?) maybe remove initial rotation
-# redundant design data
+#TODO: Remove initial rotation, redundant design data
+#WARNING: this task will delete design data!!
 export(float) var initial_rotation = 0.0
 
-#TODO:These are part of the Arc Spawn Shape
-#WARNING: this tasks will delete design data!!
+#TODO:Place these on Arc Spawn Shape
+#WARNING: this task will delete design data!!
 export(float) var radius = 1.0
 export(float) var arc_size = 360.0
 
 #TODO: Replace with a better spawn Shape selector
 export(int) var spawnShapeOption = 0
 
-#TODO: rename rotation_speed to speed to include more movement patterns
-#TODO: Possibly place this design component onto the movements directly
-#WARNING: these tasks will delete design data!!
+#TODO: Place rotation speed onto the rotation movements directly
+#WARNING: this task will delete design data!!
 export(float) var rotation_speed = 0.0
 
-#TODO: Introduce Spawn Movement Patterns
-#spawnMovementPattern
-#Types:
-#Rotator : implemented
-#Patrols : TODO
-
-#TODO: make Movement Option Picker
+#TODO: Make Movement Designer
 onready var spawn_movement: SpawnMovementPattern = SpawnMovementRotation.new()
 
-#TODO: Easier type management of spawnShape, match is *very* manual
-#TODO: Static shape builder?
+#TODO: Make Spawn Shape Designer
 onready var spawn_shape: SpawnShape = _makeSpawnShape()
 func _makeSpawnShape() -> SpawnShape:
 	match spawnShapeOption:
@@ -39,13 +31,13 @@ func _makeSpawnShape() -> SpawnShape:
 
 onready var _step: float = spawn_shape.solveSteps(spawn_point_count)
 
-#TODO: replace this enum with the better Shape Picker
+#TODO: Make Spawn Shape Designer
 enum SpawnShapes{
 	ARC, BOX,
 }
 
 func _ready():
-	#TODO: Below is a design patch until movement pattern solution
+	#TODO: Below is a bandaid until Movement Designer
 	spawn_movement.speed = rotation_speed
 	_makeSpawnPoints()
 	_setInitialRotation()
@@ -53,7 +45,7 @@ func _ready():
 func _process(delta):
 	spawn_movement.doMovement(delta)
 
-#TODO: (?) redundant design tool, can instead use inspector transforms
+#TODO: Remove initial rotation, redundant design data
 #WARNING: this tasks will delete design data!!
 func _setInitialRotation():
 	self.rotate(deg2rad(initial_rotation))
