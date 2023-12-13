17a4d0219e9bf88b550dbe92b8d1783a7cfa537c
Agreafel <25335635+Agreafel@users.noreply.github.com>
2023-12-12 17:43:29 -0800

    UnblockedPattern

--- Bullets/Scripts/SpawnShape.gd
+++ Bullets/Scripts/SpawnShape.gd
@@ -3,11 +3,13 @@ extends Node
 
 class_name SpawnShape
 
+export onready var step: float
+
 func _ready() -> void:
 	pass
 
-func getPoint(dist, factor):
-	return Vector2.ZERO * dist * factor
+func getPoint(factor):
+	return Vector2.ZERO * step * factor
 
 func solveSteps(spawn_count: int) -> float:
 	return float(spawn_count)
--- Bullets/Scripts/SpawnShapeArc.gd
+++ Bullets/Scripts/SpawnShapeArc.gd
@@ -11,8 +11,8 @@ func solveSteps(spawn_count: int)-> float:
 func _solveStepsOnArc(spawn_point_count: int) -> float:
 	return deg2rad(arc_size) / spawn_point_count
 
-func getPosition(step, factor) -> Vector2:
-	return _positionOnArc(step, factor)
+func getPosition(factor) -> Vector2:
+	return _positionOnArc(factor)
 
-func _positionOnArc(step, factor) -> Vector2:
+func _positionOnArc(factor) -> Vector2:
 	return Vector2(radius, 0).rotated(step * factor)
