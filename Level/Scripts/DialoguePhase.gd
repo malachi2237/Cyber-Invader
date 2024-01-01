b118f0d55035f13c2cc6dbf37ca2273c52058646
malachi2237 <malachi2237@gmail.com>
2024-01-01 15:51:27 -0800

    Dialgue now in testing

--- Level/Scripts/DialoguePhase.gd
+++ Level/Scripts/DialoguePhase.gd
@@ -1,5 +1,7 @@
 extends LevelPhase
 
+class_name DialoguePhase
+
 export var dialogue_node: NodePath
 
 export var character_frame_a: PackedScene
@@ -10,18 +12,31 @@ onready var dialogue: Dialogue = get_node(dialogue_node)
 onready var frame_a: DialogueFrame = character_frame_a.instance()
 onready var frame_b: DialogueFrame = character_frame_b.instance()
 
+var player: Player = null
+
 var current_frame: DialogueFrame
 
 func _ready():
 	var scene = Utility.getScene(self)
+	var test_player
 	
 	frame_a.hide()
 	frame_b.hide()
 	
 	Utility.placeInScene(scene, frame_a, null)
 	Utility.placeInScene(scene, frame_b, null)
+	
+	player = Utility.get_player(self)
 
-func advance_dialogue():
+func start_phase():
+	player.set_process_input(false)
+	_advance_dialogue()
+
+func _process(_delta):
+	if Input.is_action_pressed("ui_accept"):
+		_advance_dialogue()
+
+func _advance_dialogue():
 	var line = dialogue.consume_next_line()
 	
 	if not line:
@@ -33,7 +48,7 @@ func advance_dialogue():
 	if frame:
 		frame.set_dialogue(line.text)
 		
-		if frame.character_name != line.character_name:
+		if frame.character_name.text != line.character_name:
 			current_frame.hide()
 			frame.show()
 			current_frame = frame
@@ -48,3 +63,5 @@ func get_frame(name: String) -> Node:
 
 func _end_phase():
 	._end_phase()
+	set_process_input(true)
+	queue_free()
