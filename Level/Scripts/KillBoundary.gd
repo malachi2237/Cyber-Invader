744948039dd8a139712ee30cff195d79be57b957
malachi2237 <35237626+malachi2237@users.noreply.github.com>
2024-02-15 23:14:58 -0800

    Fixed an issue where enemies werent being deleted by kill boundary

--- Level/Scripts/KillBoundary.gd
+++ Level/Scripts/KillBoundary.gd
@@ -7,5 +7,9 @@ func _ready():
 	var _toss = self.connect("area_exited", self, "_kill")
 
 func _kill(area: Area2D):
-	area.queue_free()
+	var area_parent = area.get_parent()
+	if area_parent is Enemy:
+		area_parent.queue_free()
+	else:
+		area.queue_free()
 
